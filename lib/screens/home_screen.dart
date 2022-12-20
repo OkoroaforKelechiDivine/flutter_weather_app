8fd3207fc7aeefe6a069dfa17c9ee693d7475443
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2022-12-20 02:03:57 +0100

    fixed null safety exception

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weather_app/controllers/global_controller.dart';
+import 'package:weather_app/widgets/header_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,6 +15,20 @@ class _HomeScreenState extends State<HomeScreen> {
   final GlobalController globalController = Get.put(GlobalController(), permanent: true);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        // The text button will call the increment method defined in the controller class and
+        // Text which will show the updated value of the count.
+        child: Obx(() => globalController.checkLoading().isTrue ? const Center(
+          child: CircularProgressIndicator(),
+        )
+        : ListView(
+      scrollDirection: Axis.vertical,
+      children: const [
+        HeaderWidget(),
+      ],
+        )),
+      ),
+    );
   }
 }

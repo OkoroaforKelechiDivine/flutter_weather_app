9ef82e9e0e31a3b1c6edb9fc06b535a1160f62e1
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2023-01-06 19:05:56 +0100

    editing

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -46,6 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
     await Provider.of<WeatherProvider>(context, listen: false).getWeatherData();
   }
 
+
   @override
   Widget build(BuildContext context) {
     final weatherData = Provider.of<WeatherProvider>(context);

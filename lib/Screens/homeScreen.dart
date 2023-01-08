5a2f5f88b8b79a6d41028f056ad327dd55236eac
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2023-01-08 13:43:10 +0100

    editing

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -42,6 +42,7 @@ class _HomeScreenState extends State<HomeScreen> {
     _isLoading = false;
   }
 
+
   Future<void> _refreshData(BuildContext context) async {
     await Provider.of<WeatherProvider>(context, listen: false).getWeatherData();
   }

78b457cac902f8e1f0928ea617a41427a8c52118
OkoroaforKelechiDivine <106816493+okoroaforkelechidivine@users.noreply.github.com>
2023-01-30 14:13:20 +0100

    editing

--- lib/models/dailyWeather.dart
+++ lib/models/dailyWeather.dart
@@ -17,7 +17,6 @@ class DailyWeather with ChangeNotifier {
 
   factory DailyWeather.fromJson(Map<String, dynamic> json) {
 
-
     final precipitationData = json['daily'][0]['pop'];
     final calculatePrecipitation = precipitationData * 100;
     final precipitation = calculatePrecipitation.toStringAsFixed(0);
